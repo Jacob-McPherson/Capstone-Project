8eacb01882503627905b01ed20adb550a78f31e6
Jacob McPherson <mcphersonj092@gmail.com>
2026-03-24 12:32:15 -0500

    fixed lincoln park

--- src/Login.tsx
+++ src/Login.tsx
@@ -14,7 +14,7 @@ export default function Login() {
 const handleLogin = async () => {
   await supabase.auth.signInWithOAuth({
    provider: "google",
-   options: { redirectTo: "http://localhost:5173/home",
+   options: { redirectTo: "blueprint-taskmanager.vercel.app",
 
    } ,
   });
